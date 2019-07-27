6331f30e7f1b2efb2d17f44eda0a2ed18d6df541
ranjit23 <desairanjit233@gmail.com>
2019-07-27 18:21:13 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </div>
-      </div>
-    </Router>
-  );
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducer";
+
+import thunk from "redux-thunk";
+
+const middleware = [thunk];
+const initialState = {};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
